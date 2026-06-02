a4fb24cada7e8e1a92ba7dc2081470436b545a6b
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-02 16:40:56 +0100

    Generate constructor

---
+++ src/main/java/com/cwa/data/Staff.java
@@ -0,0 +1,15 @@
+package com.cwa.data;
+
+public class Staff {
+    private String staffName;
+    private String staffTitle;
+    private double staffSalary;
+
+    public Staff(String staffName, String staffTitle, double staffSalary) {
+        this.staffName = staffName;
+        this.staffTitle = staffTitle;
+        this.staffSalary = staffSalary;
+    }
+}
+
+ 
