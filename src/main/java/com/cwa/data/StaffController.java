fe88655c971e5a90d168eb3fe4fc352f0238530e
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-02 20:17:56 +0100

    add ListArray

--- src/main/java/com/cwa/data/StaffController.java
+++ src/main/java/com/cwa/data/StaffController.java
@@ -1,4 +1,7 @@
 package com.cwa.data;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StaffController {
 
+    List<Staff> allStaff = new ArrayList<>();
+
+
     @GetMapping("/staffdetails")
     public String getstaffdetails(Model model){
-        Staff myStaff = new Staff("Exemple","Developer",10000);
-        model.addAttribute("staff",myStaff);
+        allStaff.add(new Staff("Exemple 1","Developer",10000));
+        allStaff.add(new Staff("Exemple 2","Front-end",30000));
+        allStaff.add(new Staff("Exemple 3","BackEnd",70000));
+        model.addAttribute("allStaff",allStaff);
         return "staffdetails";
     }
     
