f568d71703548cbf7613514d01ff3a629df9d04d
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-02 16:21:59 +0100

    GetMapping

--- src/main/java/com/cwa/data/StaffController.java
+++ src/main/java/com/cwa/data/StaffController.java
@@ -1,8 +1,16 @@
 package com.cwa.data;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class StaffController {
+
+    @GetMapping("/staffnames")
+    public String getStaffName(){
+        return "staffnames";
+    }
     
 }
