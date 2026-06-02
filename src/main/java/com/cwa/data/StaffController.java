8b850a6351bbe68ab12cfcbfd903ccb65e174b08
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-02 20:27:07 +0100

    fix bug loop

--- src/main/java/com/cwa/data/StaffController.java
+++ src/main/java/com/cwa/data/StaffController.java
@@ -1,5 +1,6 @@
 package com.cwa.data;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,14 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StaffController {
 
-    List<Staff> allStaff = new ArrayList<>();
+    List<Staff> allStaff = Arrays.asList(
+        new Staff("Exemple 1","Developer",10000),
+        new Staff("Exemple 2","Front-end",30000),
+        new Staff("Exemple 3","BackEnd",70000)
+
+    );
 
 
     @GetMapping("/staffdetails")
     public String getstaffdetails(Model model){
-        allStaff.add(new Staff("Exemple 1","Developer",10000));
-        allStaff.add(new Staff("Exemple 2","Front-end",30000));
-        allStaff.add(new Staff("Exemple 3","BackEnd",70000));
         model.addAttribute("allStaff",allStaff);
         return "staffdetails";
     }
