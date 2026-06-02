9718db9951b862298de160a75a88dd033d173160
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-02 16:46:07 +0100

    Get & Set

--- src/main/java/com/cwa/data/Staff.java
+++ src/main/java/com/cwa/data/Staff.java
@@ -10,6 +10,35 @@ public class Staff {
         this.staffTitle = staffTitle;
         this.staffSalary = staffSalary;
     }
+        public Staff() {
+    }
+
+    public String getStaffName() {
+        return this.staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public String getStaffTitle() {
+        return this.staffTitle;
+    }
+
+    public void setStaffTitle(String staffTitle) {
+        this.staffTitle = staffTitle;
+    }
+
+    public double getStaffSalary() {
+        return this.staffSalary;
+    }
+
+    public void setStaffSalary(double staffSalary) {
+        this.staffSalary = staffSalary;
+    }
+    
 }
+
+
 
  
