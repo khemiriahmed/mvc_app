b22b640b1293f1d1bf45013018ba2f3c77a6a174
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-04 17:16:54 +0100

    Formatted

--- src/main/java/com/cwa/data/StaffController.java
+++ src/main/java/com/cwa/data/StaffController.java
@@ -1,4 +1,5 @@
 package com.cwa.data;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,23 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 public class StaffController {
 
     List<Staff> allStaff = Arrays.asList(
-        new Staff("Exemple 1","Developer",10000),
-        new Staff("Exemple 2","Front-end",30000),
-        new Staff("Exemple 3","BackEnd",70000)
+            new Staff("Exemple 1", "Developer", 10000),
+            new Staff("Exemple 2", "Front-end", 30000),
+            new Staff("Exemple 3", "BackEnd", 70000)
 
     );
 
-
     @GetMapping("/staffdetails")
-    public String getstaffdetails(Model model){
-        model.addAttribute("allStaff",allStaff);
+    public String getstaffdetails(Model model) {
+        model.addAttribute("allStaff", allStaff);
         return "staffdetails";
     }
-    
+
 }
