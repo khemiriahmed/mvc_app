c44d37be7f12e9f0cdae9722d458ae0604b4d9d2
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-06 02:34:01 +0100

    Form Controller

--- src/main/java/com/cwa/data/FormController.java
+++ src/main/java/com/cwa/data/FormController.java
@@ -1,21 +1,37 @@
 package com.cwa.data;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class FormController {
 
-    Staff myStaff = new Staff("Exemple 4", "Developer", 10000);
-            
+    // Staff myStaff = new Staff("Exemple 4", "Developer", 10000);
+    List<Staff> AllStaff = new ArrayList<>();
 
-
-    @GetMapping("/form")
-    public String getstaffForm(Model model) {
-        model.addAttribute("Staff", myStaff);
+    @GetMapping("/")
+    public String addNewStaff(Model model) {
+        Staff myStaff = new Staff();
+        model.addAttribute("addNewStaff", myStaff);
         return "staffform";
+    }
+
+    @PostMapping("/dataSubmitForm")
+    public String dataSubmitForm(Staff staff) {
+        AllStaff.add(staff);
+        return "redirect:/getallstaff";
+    }
+
+    @GetMapping("/getallstaff")
+    public String getAllstaff(Model model) {
+         model.addAttribute("AllStaff", AllStaff);
+        return "getallstaff";
     }
 
 }
